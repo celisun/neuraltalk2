c081819fab0e13cfa4d42a58e2b0bc22f2e554f3
celishxxp <cs2393@cornell.edu>
2018-12-06 00:37:13 -0500

    preprocess

--- prepro.py
+++ prepro.py
@@ -11,7 +11,7 @@ This script reads this json, does some basic preprocessing on the captions
 
 Output: a json file and an hdf5 file
 The hdf5 file contains several fields:
-/images is (N,3,256,256) uint8 array of raw image data in RGB format
+/images is (N,3,224,224) uint8 array of raw image data in RGB format   :: we change to 224, 224
 /labels is (M,max_length) uint32 array of encoded labels, zero padded
 /label_start_ix and /label_end_ix are (N,) uint32 arrays of pointers to the 
   first and last indices (in range 1..M) of labels for each image
@@ -36,13 +36,15 @@ from scipy.misc import imread, imresize
 def prepro_captions(imgs):
   
   # preprocess all the captions
-  print 'example processed tokens:'
+  print ('example processed tokens:')
   for i,img in enumerate(imgs):
     img['processed_tokens'] = []
     for j,s in enumerate(img['captions']):
-      txt = str(s).lower().translate(None, string.punctuation).strip().split()
+      translator = str.maketrans('', '', string.punctuation)
+      txt = str(s).lower().translate(translator).strip().split()
       img['processed_tokens'].append(txt)
-      if i < 10 and j == 0: print txt
+      if i < 10 and j == 0: print (txt)
+
 
 def build_vocab(imgs, params):
   count_thr = params['word_count_threshold']
@@ -53,19 +55,19 @@ def build_vocab(imgs, params):
     for txt in img['processed_tokens']:
       for w in txt:
         counts[w] = counts.get(w, 0) + 1
-  cw = sorted([(count,w) for w,count in counts.iteritems()], reverse=True)
-  print 'top words and their counts:'
-  print '\n'.join(map(str,cw[:20]))
+  cw = sorted([(count,w) for w,count in counts.items()], reverse=True)
+  print ('top words and their counts:')
+  print ('\n'.join(map(str,cw[:20])))
 
   # print some stats
-  total_words = sum(counts.itervalues())
-  print 'total words:', total_words
-  bad_words = [w for w,n in counts.iteritems() if n <= count_thr]
-  vocab = [w for w,n in counts.iteritems() if n > count_thr]
+  total_words = sum(counts.values())
+  print ('total words:', total_words)
+  bad_words = [w for w,n in counts.items() if n <= count_thr]
+  vocab = [w for w,n in counts.items() if n > count_thr]
   bad_count = sum(counts[w] for w in bad_words)
-  print 'number of bad words: %d/%d = %.2f%%' % (len(bad_words), len(counts), len(bad_words)*100.0/len(counts))
-  print 'number of words in vocab would be %d' % (len(vocab), )
-  print 'number of UNKs: %d/%d = %.2f%%' % (bad_count, total_words, bad_count*100.0/total_words)
+  print ('number of bad words: %d/%d = %.2f%%' % (len(bad_words), len(counts), len(bad_words)*100.0/len(counts)))
+  print ('number of words in vocab would be %d' % (len(vocab), ))
+  print ('number of UNKs: %d/%d = %.2f%%' % (bad_count, total_words, bad_count*100.0/total_words))
 
   # lets look at the distribution of lengths as well
   sent_lengths = {}
@@ -74,16 +76,16 @@ def build_vocab(imgs, params):
       nw = len(txt)
       sent_lengths[nw] = sent_lengths.get(nw, 0) + 1
   max_len = max(sent_lengths.keys())
-  print 'max length sentence in raw data: ', max_len
-  print 'sentence length distribution (count, number of words):'
+  print ('max length sentence in raw data: ', max_len)
+  print ('sentence length distribution (count, number of words):')
   sum_len = sum(sent_lengths.values())
-  for i in xrange(max_len+1):
-    print '%2d: %10d   %f%%' % (i, sent_lengths.get(i,0), sent_lengths.get(i,0)*100.0/sum_len)
+  for i in range(max_len+1):
+    print ('%2d: %10d   %f%%' % (i, sent_lengths.get(i,0), sent_lengths.get(i,0)*100.0/sum_len))
 
   # lets now produce the final annotations
   if bad_count > 0:
     # additional special UNK token we will use below to map infrequent words to
-    print 'inserting the special UNK token'
+    print('inserting the special UNK token')
     vocab.append('UNK')
   
   for img in imgs:
@@ -106,7 +108,7 @@ def assign_splits(imgs, params):
       else: 
         img['split'] = 'train'
 
-  print 'assigned %d to val, %d to test.' % (num_val, num_test)
+  print ('assigned %d to val, %d to test.' % (num_val, num_test))
 
 def encode_captions(imgs, params, wtoi):
   """ 
@@ -147,9 +149,10 @@ def encode_captions(imgs, params, wtoi):
   
   L = np.concatenate(label_arrays, axis=0) # put all the labels together
   assert L.shape[0] == M, 'lengths don\'t match? that\'s weird'
+  # print (label_length)
   assert np.all(label_length > 0), 'error: some caption had no words?'
 
-  print 'encoded captions to array of size ', `L.shape`
+  print ('encoded captions to array of size: %{} '.format(L.shape))
   return L, label_start_ix, label_end_ix, label_length
 
 def main(params):
@@ -179,27 +182,27 @@ def main(params):
   f.create_dataset("label_start_ix", dtype='uint32', data=label_start_ix)
   f.create_dataset("label_end_ix", dtype='uint32', data=label_end_ix)
   f.create_dataset("label_length", dtype='uint32', data=label_length)
-  dset = f.create_dataset("images", (N,3,256,256), dtype='uint8') # space for resized images
+  dset = f.create_dataset("images", (N,3,224,224), dtype='uint8') # space for resized images
   for i,img in enumerate(imgs):
     # load the image
     I = imread(os.path.join(params['images_root'], img['file_path']))
     try:
-        Ir = imresize(I, (256,256))
+        Ir = imresize(I, (224,224))
     except:
-        print 'failed resizing image %s - see http://git.io/vBIE0' % (img['file_path'],)
+        print ('failed resizing image %s - see http://git.io/vBIE0' % (img['file_path'],))
         raise
     # handle grayscale input images
     if len(Ir.shape) == 2:
       Ir = Ir[:,:,np.newaxis]
       Ir = np.concatenate((Ir,Ir,Ir), axis=2)
-    # and swap order of axes from (256,256,3) to (3,256,256)
+    # and swap order of axes from (224,224,3) to (3,224,224)
     Ir = Ir.transpose(2,0,1)
     # write to h5
     dset[i] = Ir
     if i % 1000 == 0:
-      print 'processing %d/%d (%.2f%% done)' % (i, N, i*100.0/N)
+      print ('processing %d/%d (%.2f%% done)' % (i, N, i*100.0/N))
   f.close()
-  print 'wrote ', params['output_h5']
+  print ('wrote ', params['output_h5'])
 
   # create output json file
   out = {}
@@ -215,26 +218,26 @@ def main(params):
     out['images'].append(jimg)
   
   json.dump(out, open(params['output_json'], 'w'))
-  print 'wrote ', params['output_json']
+  print ('wrote ', params['output_json'])
 
 if __name__ == "__main__":
 
   parser = argparse.ArgumentParser()
 
   # input json
-  parser.add_argument('--input_json', required=True, help='input json file to process into hdf5')
-  parser.add_argument('--num_val', required=True, type=int, help='number of images to assign to validation data (for CV etc)')
+  parser.add_argument('--input_json', default=os.path.join("data", "data_raw.json"), help='input json file to process into hdf5')
+  parser.add_argument('--num_val', default=1000, type=int, help='number of images to assign to validation data (for CV etc)')
   parser.add_argument('--output_json', default='data.json', help='output json file')
   parser.add_argument('--output_h5', default='data.h5', help='output h5 file')
   
   # options
   parser.add_argument('--max_length', default=16, type=int, help='max length of a caption, in number of words. captions longer than this get clipped.')
-  parser.add_argument('--images_root', default='', help='root location in which images are stored, to be prepended to file_path in input json')
+  parser.add_argument('--images_root', default='data', help='root location in which images are stored, to be prepended to file_path in input json')
   parser.add_argument('--word_count_threshold', default=5, type=int, help='only words that occur more than this number of times will be put in vocab')
   parser.add_argument('--num_test', default=0, type=int, help='number of test images (to withold until very very end)')
 
   args = parser.parse_args()
   params = vars(args) # convert to ordinary dict
-  print 'parsed input parameters:'
-  print json.dumps(params, indent = 2)
+  print ('parsed input parameters:')
+  print (json.dumps(params, indent = 2))
   main(params)
